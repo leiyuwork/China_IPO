8b57eb7cacb32376d13d6753331bfe8920442802
leiyuwork <g51583255>
2021-04-14 19:05:18 +0900

    Initial commit

---
+++ Prospectus.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    'Accept': 'text/html, */*; q=0.01',
+    'Referer': 'http://tv.cctv.com/lm/xwlb/',
+    'X-Requested-With': 'XMLHttpRequest',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+}
