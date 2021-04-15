94b9a3c47c5505f27028b4cae7768f771876488a
leiyuwork <g51583255>
2021-04-15 10:50:18 +0900

    从金融界网站换到了东方财富网

--- Prospectus.py
+++ Prospectus.py
@@ -9,20 +9,15 @@ headers = {
     'X-Requested-With': 'XMLHttpRequest',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
 }
-for code in range(300001, 300586):
+for code in range(300219, 300586):
     print('*****************' + str(code) + '*****************')
-    response_1 = requests.get('http://stock.jrj.com.cn/share,' + str(code) +',zgsms.shtml?to=pc', headers=headers)
-    time.sleep(5)
-    response_1.encoding = 'gb18030'
-    bs_obj = BeautifulSoup(response_1.text, 'lxml')
-    linkSpan = bs_obj.find_all('ul', {'class': 'newlist'})
+    response = requests.get('https://data.eastmoney.com/xg/xg/detail/' + str(code) +'.html', headers=headers)
+    time.sleep(3)
+    bs_obj = BeautifulSoup(response.text, 'lxml')
+    linkSpan = bs_obj.select('body > div.main > div:nth-child(12) > div > div.fr')
+    print(linkSpan)
     for a in linkSpan:
-        response_2 = requests.get("http://stock.jrj.com.cn/" + a.find('a', href=True).attrs['href'])
-        response_2.encoding = 'gb18030'
-        soup = BeautifulSoup(response_2.text, 'lxml')
-        Span = soup.select('body > div.body > div.warp > div.main > table > tbody > tr > td.m > div.tabs2.txt > p.tc')
-        for s in Span:
-            filename = os.path.join(r'C:\Users\Ray94\Desktop\ipo', str(code)+'.pdf')
-            with open(filename, 'wb') as f:
-                f.write(requests.get(s.find('a', href=True).attrs['href']).content)
+        filename = os.path.join(r'C:\Users\Ray94\Desktop\ipo', str(code)+'.pdf')
+        with open(filename, 'wb') as f:
+            f.write(requests.get("http:" + a.find('a', href=True).attrs['href']).content)
 
